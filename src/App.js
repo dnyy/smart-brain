5227259389d11b918e2d75971543192e94d0147e
Daniel Toll <daniel_toll@hotmail.com>
2019-09-24 05:29:32 +0200

    add more stuff to profile

--- src/App.js
+++ src/App.js
@@ -27,8 +27,8 @@ const initialState = {
   input: '',
   imageUrl: '',
   boxes: [],
-  route: 'home',
-  isSignedIn: true,
+  route: 'signing',
+  isSignedIn: false,
   isProfileOpen: false,
   user: {
     id: '',
