77c5c53afeb0f897d392008644366f46b296264f
Daniel Toll <daniel_toll@hotmail.com>
2019-09-25 05:48:52 +0200

    updated profile to display input name

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Profile.css';
 
-const Profile = ({ isProfileOpen, toggleModal}) => {
+const Profile = ({ isProfileOpen, toggleModal }) => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [pet, setPet] = useState('');
+
   return (
     <div className="profile-modal">
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
@@ -11,14 +15,15 @@ const Profile = ({ isProfileOpen, toggleModal}) => {
             src="http://tachyons.io/img/logo.jpg"
             className=" h3 w3 dib" alt="avatar"
           />
-          <h1>Boaty McBoatface</h1>
+          <h1>{name}</h1>
           <h4>Images submitted: 5</h4>
           <p>Member since: May</p>
           <hr/>
           <label className="mt2 fw6" htmlFor="user-name">Name:</label>
           <input
+            onChange={e => setName(e.target.value)}
             className="pa2 ba w-100"
-            placeholder="John"
+            placeholder={name}
             type="text"
             name="user-name"
             id="name"
@@ -26,7 +31,7 @@ const Profile = ({ isProfileOpen, toggleModal}) => {
           <label className="mt2 fw6" htmlFor="user-name">Age:</label>
           <input
             className="pa2 ba w-100"
-            placeholder="56"
+            placeholder={age}
             type="text"
             name="user-age"
             id="age"
@@ -34,7 +39,7 @@ const Profile = ({ isProfileOpen, toggleModal}) => {
           <label className="mt2 fw6" htmlFor="user-name">Pet:</label>
           <input
             className="pa2 ba w-100"
-            placeholder="Dragon"
+            placeholder={pet}
             type="text"
             name="user-pet"
             id="pet"
